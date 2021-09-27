358e8b5e10ac7d399af1c517a34d455a6cc1893d
Taavi <juomakuski@hotmail.com>
2021-09-27 19:11:35 +0300

    celshading shadows fixed

--- Project P Shader Test/Assets/CelShaders/Lightning.hlsl
+++ Project P Shader Test/Assets/CelShaders/Lightning.hlsl
@@ -1,19 +1,25 @@
 void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color,
-    out float DistanceAtten, out float ShadowAtten)
-{
-#ifdef SHADERGRAPH_PREVIEW
-    Direction = normalize(float3(0.5f, 0.5f, 0.25f));
-    Color = float3(1.0f, 1.0f, 1.0f);
-    DistanceAtten = 1.0f;
-    ShadowAtten = 1.0f;
-#else
-    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
-    Light mainLight = GetMainLight(shadowCoord);
+    out float DistanceAtten, out float ShadowAtten) {
 
+#ifdef SHADERGRAPH_PREVIEW
+    Direction = normalize(float3(1, 1, -0.4));
+    Color = float4(1, 1, 1, 1);
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+    Light mainLight = GetMainLight();
     Direction = mainLight.direction;
     Color = mainLight.color;
     DistanceAtten = mainLight.distanceAttenuation;
-    ShadowAtten = mainLight.shadowAttenuation;
+
+    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    // or if you want shadow cascades :
+    // half cascadeIndex = ComputeCascadeIndex(WorldPos);
+    // float4 shadowCoord = mul(_MainLightWorldToShadow[cascadeIndex], float4(WorldPos, 1.0));
+
+    ShadowSamplingData shadowSamplingData = GetMainLightShadowSamplingData();
+    float shadowStrength = GetMainLightShadowStrength();
+    ShadowAtten = SampleShadowmap(shadowCoord, TEXTURE2D_ARGS(_MainLightShadowmapTexture, sampler_MainLightShadowmapTexture), shadowSamplingData, shadowStrength, false);
 #endif
 }
 
